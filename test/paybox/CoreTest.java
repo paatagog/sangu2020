57a92528bc08fd1fe305afe648c44a397f31e1c2
paatagog <paatagog@gmail.com>
2020-12-03 14:51:58 +0400

    დავიწყეთ ტესტირების ფრეიმვორკის შესწავლა

---
+++ test/paybox/CoreTest.java
@@ -0,0 +1,34 @@
+package paybox;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoreTest {
+
+    Core core = null;
+
+    @BeforeEach
+    void setUp() {
+        core = new Core();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void loadPayBoxConf() {
+        core.loadPayBoxConf("./payboxconfTest.xml");
+        assertEquals("1", core.getId());
+        assertEquals("საცდელი მისამართი", core.getAddress());
+        assertEquals("599-123456", core.getSupportNumber());
+    }
+
+    @Test
+    void loadServices() {
+    }
+
+}
